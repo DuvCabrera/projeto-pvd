c414f9c2ed1d5d7fbfb8039f41ad45989ae55cb7
DuvCabrera <eduardoviniciuscabrera@gmail.com>
2022-05-28 17:10:54 -0300

    moved the classes tested to its production folders

--- projeto_pvd/lib/src/product_module/external/repositories/update_product_repository.dart
+++ projeto_pvd/lib/src/product_module/external/repositories/update_product_repository.dart
@@ -1,1 +1,20 @@
-// TODO Implement this library.
+import '../../infra/infra.dart';
+import '../external.dart';
+
+class UpdateProductRepository extends IUpdateProductRepository {
+  final IProductDatasource datasource;
+
+  UpdateProductRepository(this.datasource);
+
+  @override
+  Future<void> update(
+      {required int id,
+      required Map<String, dynamic> data,
+      required String tableName}) async {
+    try {
+      await datasource.update(data: data, tableName: tableName, id: id);
+    } catch (e) {
+      throw ExternalError.unexpected;
+    }
+  }
+}
